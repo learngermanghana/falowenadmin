424e69eadde8c96a35c43bf2f81be707504d214e
learngermanghana <learngermanghana@gmail.com>
2026-07-01 12:10:06 +0000

    Fix Live Classes build syntax

--- src/pages/LiveClassesPage.js
+++ src/pages/LiveClassesPage.js
@@ -1,6 +1,12 @@
+import React from "react";
 import LiveClassesPageV2 from "./LiveClassesPageV2.jsx";
 import GhanaDateTimeLocalInjector from "../components/GhanaDateTimeLocalInjector.jsx";
 
 export default function LiveClassesPage() {
-  return <><GhanaDateTimeLocalInjector /><LiveClassesPageV2 /></>;
+  return React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(GhanaDateTimeLocalInjector),
+    React.createElement(LiveClassesPageV2),
+  );
 }
