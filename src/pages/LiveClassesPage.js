559b7a9591dc97949f1ad9570893e653b2545e6d
learngermanghana <learngermanghana@gmail.com>
2026-07-14 10:27:09 +0000

    Show lesson date repair above live classes

--- src/pages/LiveClassesPage.js
+++ src/pages/LiveClassesPage.js
@@ -1,12 +1,14 @@
 import React from "react";
-import LiveClassesPageV2 from "./LiveClassesPageV2.jsx";
+import LiveClassLessonDateRepair from "../components/LiveClassLessonDateRepair.jsx";
 import GhanaDateTimeLocalInjector from "../components/GhanaDateTimeLocalInjector.jsx";
+import LiveClassesPageV2 from "./LiveClassesPageV2.jsx";
 
 export default function LiveClassesPage() {
   return React.createElement(
     React.Fragment,
     null,
     React.createElement(GhanaDateTimeLocalInjector),
+    React.createElement(LiveClassLessonDateRepair),
     React.createElement(LiveClassesPageV2),
   );
 }
