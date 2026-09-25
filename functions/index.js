72e987e019502b855d9aca4d4f2ddf1a49d2057f
learngermanghana <learngermanghana@gmail.com>
2026-09-25 09:09:01 +0000

    Wire trial access email workers

--- functions/index.js
+++ functions/index.js
@@ -13,7 +13,7 @@ const { defineSecret } = require("firebase-functions/params");
 const { createAttendanceConfirmationEmailJob, sendAssignmentAttendanceCreditEmail } = require("./attendanceConfirmationEmails.js");
 const { retryFailedAttendanceDeliveries, listAttendanceDeliveryHealth } = require("./attendanceConfirmationRetry.js");
 const { registerCompletionDocumentRoute } = require("./completionParticipationDocument.js");
-const { createRegistrationLifecycleTriggers } = require("./registrationLifecycleEvents.js");
+const { createRegistrationLifecycleTriggers } = require("./registrationLifecycleEvents.js");\nconst { createTrialAccessWelcomeEmailTrigger, createTrialAccessReminderEmailJob } = require("./trialAccessEmails.js");
 const { assignmentAttendanceEligibility } = require("./assignmentAttendanceEligibility.js");
 const {
   HOLIDAY_NOTICE_PROTOCOL_VERSION,
@@ -2300,6 +2300,20 @@ const registrationLifecycleTriggers = createRegistrationLifecycleTriggers({
 
 exports.trackStudentRegistrationReceived = registrationLifecycleTriggers.registrationReceived;
 exports.sendEnrollmentConfirmationDocuments = registrationLifecycleTriggers.enrollmentConfirmed;
+
+exports.sendTrialAccessWelcomeEmail = createTrialAccessWelcomeEmailTrigger({
+  admin,
+  db,
+  onDocumentCreated,
+  runtimeConfig,
+});
+
+exports.sendTrialAccessReminderEmails = createTrialAccessReminderEmailJob({
+  admin,
+  db,
+  onSchedule,
+  runtimeConfig,
+});
 
 exports.createFlatSubmissionMarkingJob = onDocumentCreated("submissions/{submissionId}", async (event) => {
   await createAutomaticMarkingJob(event, "flat");
